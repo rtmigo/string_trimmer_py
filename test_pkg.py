b0986d9dd5aeee4ac773ab4483c6df2cecdb400b
Artem IG <ortemeo@gmail.com>
2022-01-05 00:11:27 +0300

    upd

--- test_pkg.py
+++ test_pkg.py
@@ -2,7 +2,7 @@ from chkpkg import Package
 
 if __name__ == "__main__":
     with Package() as pkg:
-        pkg.run_python_code('from text2words import WordExtractor')
+        pkg.run_python_code('from string_trimmer import SuffixTrimmer, PrefixTrimmer, TripleTrimmer')
 
     print("\nPackage is OK!")
 
